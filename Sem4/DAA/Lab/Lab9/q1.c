a2709543386053d1c2bc9e0299d319b3e459589b
adityasinha347 <adityasinha347@gmail.com>
2025-03-11 00:45:35 +0530

    daa lab9

---
+++ Sem4/DAA/Lab/Lab9/q1.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void makeShift(char*text,char*pattern,int shift[]){
+    int m=strlen(pattern);
+    int n=strlen(text);
+
+    for(int i=0;i<256;i++)
+        shift[i]=m;
+
+    for(int i=0;i<m-1;i++)
+        shift[(unsigned char)pattern[i]]=m-i-1;
+}
+
+int stringMatching(char*text,char*pattern){
+    int shift[256];
+    int m=strlen(pattern);
+    int n=strlen(text);
+    makeShift(text,pattern,shift);
+
+    int i=m-1;
+    while(i<n){
+        int k=0;
+        while(k<m && pattern[m-1-k] == text[i-k])
+            k++;
+
+        if(k==m)return i-m+1;
+        i+=shift[(unsigned char)text[i]];
+    }
+    return -1;
+}
+
+int main(){
+    char text[100],pattern[100];
+    printf("Enter the text: ");
+    scanf("%s",text);
+    printf("Enter the pattern: ");
+    scanf("%s",pattern);
+
+    int pos=stringMatching(text,pattern);
+    if(pos!=-1)
+        printf("Pattern found at position %d\n",pos);
+    else
+        printf("Pattern not found\n");
+    return 0;
+}
