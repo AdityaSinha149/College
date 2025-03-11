3d6b1b187d8eed463f0cf19a2b288bed38e241ae
adityasinha347 <adityasinha347@gmail.com>
2025-03-11 13:21:45 +0530

    daa lab9

--- Sem4/DAA/Lab/Lab9/q2.c
+++ Sem4/DAA/Lab/Lab9/q2.c
@@ -1,69 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    char data;
-    struct node *next;
-}node;
+int opcount = 0;
 
-node createnode(node* newnode,char key){
-    newnode=(node*)malloc(sizeof(node));
-    newnode->data=key;
-    newnode->next=NULL;
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+
+node* createnode(int key) {
+    node *newnode = (node*)malloc(sizeof(node));
+    newnode->data = key;
+    newnode->next = NULL;
     return newnode;
 }
 
-void insert(node* hashmap[],int m,char key){
-    int index=key%m;
-    node* newnode=createnode(newnode,key);    
+void insert(node* hashmap[], int m, int key) {
+    int index = key % m;
+    node* newnode = createnode(key);
 
-    if(hashmap[index]==NULL)
-        hashmap[index]=newnode;
-    else{
-        node* temp=hashmap[index];
-        while(temp->next!=NULL)
-            temp=temp->next;
-        temp->next=newnode;
+    if (hashmap[index] == NULL)
+        hashmap[index] = newnode;
+    else {
+        node* temp = hashmap[index];
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = newnode;
     }
 }
 
-int search(node* hashmap[],int m,char key){
-    int index=key%m;
-    node* temp=hashmap[index];
-    while(temp!=NULL){
-        if(temp->data==key)
+int search(node* hashmap[], int m, int key) {
+    int index = key % m;
+    node* temp = hashmap[index];
+    while (temp != NULL) {
+        opcount++;
+        if (temp->data == key)
             return 1;
-        temp=temp->next;
+        temp = temp->next;
     }
     return 0;
 }
 
-insert elements in while(1) untill -1
-int main(){
+int main() {
     int m;
     printf("Enter the size of the hash table: ");
-    scanf("%d",&m);
-    node* hashmap[m];
-    for(int i=0;i<m;i++)
-        hashmap[i]=NULL;
+    scanf("%d", &m);
 
-    while(1){
+    node** hashmap = (node**)malloc(m * sizeof(node*));
+    for (int i = 0; i < m; i++)
+        hashmap[i] = NULL;
+
+    while (1) {
         int key;
-        printf("Enter the element to insert(-1 to stop): ");
-        scanf("%d",&key);
-        if(key==-1)
+        printf("Enter the integer to insert (-1 to stop): ");
+        scanf("%d", &key);
+        if (key == -1)
             break;
-        insert(hashmap,m,key);
+        insert(hashmap, m, key);
     }
 
     int key;
-    printf("Enter the element to search(-1 to stop): ");
-    scanf("%d",&key);
-    if(key==-1)
-        break;
-    if(search(hashmap,m,key))
+    printf("Enter the integer to search: ");
+    scanf("%d", &key);
+    if (search(hashmap, m, key))
         printf("Element found\n");
     else
         printf("Element not found\n");
+
+    for (int i = 0; i < m; i++) {
+        node* temp = hashmap[i];
+        while (temp != NULL) {
+            node* toDelete = temp;
+            temp = temp->next;
+            free(toDelete);
+        }
+    }
+    free(hashmap);
+    
+    printf("opcount:%d",opcount);
+
     return 0;
 }
