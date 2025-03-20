a38c6de08f4d9e1eab3de0a17fa73e68cabf72b1
Aditya Sinha <155882262+AdityaSinha149@users.noreply.github.com>
2025-03-20 16:13:27 +0530

    Update l7q1.c

--- Sem4/ES/Lab/Lab7/l7q1.c
+++ Sem4/ES/Lab/Lab7/l7q1.c
@@ -1,7 +1,8 @@
 #include <lpc17xx.h>
 
 unsigned char tohex[4] = {0x06, 0x5B, 0x4F, 0x66};
-unsigned int pos={0<<23,1<<23,2<<23,3<<23};
+unsigned int pos[4]={0<<23,1<<23,2<<23,3<<23};
+int i,j;
 
 void configure_7seg() {
     LPC_GPIO0->FIODIR |= 0XFF0;
@@ -16,11 +17,11 @@ int main()
 
     while (1)
     {
-        for (int i = 0; i < 4; i++)
+        for (i = 0; i < 4; i++)
         {
-            LPC_GPIO0->FIOPIN = pos[i];
-            LPC_GPIO1->FIOPIN = tohex[4-i];
-            for (int j = 0; j < 1000; j++);
+            LPC_GPIO1->FIOPIN = pos[i];
+            LPC_GPIO0->FIOPIN = tohex[3-i]<<4;
+            for (j = 0; j < 1000; j++);
         }
     }
     return 0;
