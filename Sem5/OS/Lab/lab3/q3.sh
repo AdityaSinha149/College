04a4e77a8460c425b42f177fbcafec6383d4045a
Aditya Sinha <adityasinha347@gmail.com>
2025-08-05 09:50:23 +0530

    lab 3 q3 correction from os lab

--- Sem5/OS/Lab/lab3/q3.sh
+++ Sem5/OS/Lab/lab3/q3.sh
@@ -12,8 +12,8 @@ fi
 
 case $condition in
   1)  # d > 0
-      r1=$(echo "scale=2; (-$b + sqrt($d)) / (2 * $a)" | bc -l)
-      r2=$(echo "scale=2; (-$b - sqrt($d)) / (2 * $a)" | bc -l)
+      r1=$(echo "scale=2; ((-1)*$b + sqrt($d)) / (2 * $a)" | bc -l)
+      r2=$(echo "scale=2; ((-1)*$b - sqrt($d)) / (2 * $a)" | bc -l)
       echo "Real & distinct roots: $r1 and $r2"
       ;;
   0)  # d = 0
@@ -21,9 +21,8 @@ case $condition in
       echo "Real & equal root: $r"
       ;;
  -1)  # d < 0
-      d_pos=$(( -d ))
       real=$(echo "scale=2; (-1)*$b / (2 * $a)" | bc -l)
-      imag=$(echo "scale=2; sqrt($d_pos) / (2 * $a)" | bc -l)
+      imag=$(echo "scale=2; sqrt((-1)*$d) / (2 * $a)" | bc -l)
       echo "Complex roots: $real+${imag}i and $real-${imag}i"
       ;;
 esac
