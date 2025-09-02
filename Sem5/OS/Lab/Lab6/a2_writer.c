0ef9873aee0819c40710272eea5b5e8837b9fe3c
Aditya Sinha <adityasinha347@gmail.com>
2025-09-02 11:31:59 +0530

    additional 2 from lab
    its so wrong gpt did me wrong

---
+++ Sem5/OS/Lab/Lab6/a2_writer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define FIFO_REQ "/tmp/fifo_req"
+#define FIFO_RSP "/tmp/fifo_rsp"
+
+typedef struct {
+    char type;  // 'R' or 'W'
+    int action; // 1=request, 0=release
+    int pid;
+} message;
+
+int main() {
+    int fd_req = open(FIFO_REQ, O_WRONLY);
+    int fd_rsp = open(FIFO_RSP, O_RDONLY);
+    if (fd_req == -1 || fd_rsp == -1) { perror("open"); exit(1); }
+
+    message msg = {'W', 1, getpid()};
+    write(fd_req, &msg, sizeof(msg));
+
+    int ack;
+    read(fd_rsp, &ack, sizeof(ack));
+    printf("Writer %d: Writing...\n", getpid());
+    sleep(15);
+
+    msg.action = 0;
+    write(fd_req, &msg, sizeof(msg));
+    printf("Writer %d: Done writing.\n", getpid());
+
+    close(fd_req);
+    close(fd_rsp);
+    return 0;
+}
