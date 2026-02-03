a99f2af6562c5bc335619e7ede6829c00585041b
Aditya Sinha <adityasinha347@gmail.com>
2026-02-03 10:16:40 +0530

    cd lab 5 from lab

---
+++ Sem6/CD/Lab/Lab5/file.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 44e-5
+
+
+int ahbs;
+char func1(int ch, char* s){
+    int C = 0;
+    int x = 9;
+    return 4;
+}
+
+int main(){
+                     if(PI) printf("Nothing here\n");
+                     int a = 5;
+                     char b = 7;
+                     int v,k;
+                    scanf("%d", &v);
+                     //bcsVFEDGBERTbdsjkh
+                     /*vhjskdvk
+                     SFGDFHDF
+                     bdfgbnfgdn*/
+    printf          ("Demo fi                 le\n");
+        return 0;
+}
